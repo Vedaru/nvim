d86f1841d4fb2360c674d6d72e905a531d114527
Vedaru <l2859794@gmail.com>
2026-06-30 18:42:33 +0800

    chore:Update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,20 @@
 --~/.config/nvim/lua/plugins/lsp.lua
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    diagnostics = {
-      -- 去掉下划线，保留其他提示（sign/s虚浮窗）
-      underline = false,
-      -- 或只去掉 Error 的下划线，保留 Warning 等
-      -- underline = { severity = { min = vim.diagnostic.severity.WARN } },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        -- 去掉下划线，保留其他提示（sign/s虚浮窗）
+        underline = false,
+        -- 或只去掉 Error 的下划线，保留 Warning 等
+        -- underline = { severity = { min = vim.diagnostic.severity.WARN } },
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      automatic_installation = false,
     },
   },
 }
