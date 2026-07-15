7d7fb19926ca823840e3caceb0954f785fb3b540
Vedaru <l2859794@gmail>
2026-07-15 20:02:18 +0800

    feat(gutentags): restore with clean consolidated config

---
+++ lua/plugins/gutentags.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "ludovicchabant/vim-gutentags",
+    dir = vim.fn.stdpath("data") .. "/site/pack/plugins/start/vim-gutentags",
+    lazy = false,
+    priority = 500,
+    init = function()
+      local cache = vim.fn.stdpath("cache") .. "/gutentags"
+
+      vim.g.gutentags_ctags_executable = vim.fn.expand("~/.local/bin/ctags")
+      vim.g.gutentags_cache_dir = cache
+      vim.g.gutentags_ctags_tagfile = ".tags"
+      vim.g.gutentags_project_root = { ".git", ".hg", ".svn" }
+
+      vim.g.gutentags_generate_on_new = true
+      vim.g.gutentags_generate_on_missing = true
+      vim.g.gutentags_generate_on_write = true
+      vim.g.gutentags_generate_on_empty_buffer = false
+
+      vim.g.gutentags_file_list_command = "rg --files"
+      vim.g.gutentags_ctags_exclude = {
+        "node_modules", ".git", "dist", "build", "target",
+        ".next", ".nuxt", "coverage", "__pycache__",
+        "*.min.js", "*.min.css", "*.lock", "*.lockb",
+      }
+
+      vim.g.gutentags_exclude_filetypes = {
+        "gitcommit", "gitrebase", "help", "markdown",
+        "text", "startify", "fugitive", "fugitiveblame",
+      }
+
+      vim.g.gutentags_ctags_extra_args = {
+        "--fields=+lnS",
+        "--extras=+q",
+        "--output-format=e-ctags",
+      }
+
+      vim.opt.tags:prepend(cache .. "/*")
+    end,
+    config = function()
+      vim.cmd("source " .. vim.fn.stdpath("data")
+        .. "/site/pack/plugins/start/vim-gutentags/plugin/gutentags.vim")
+    end,
+  },
+}
