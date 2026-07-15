826ef48d81189c5de2f32f5a815434e3cc49087c
Vedaru <l2859794@gmail>
2026-07-15 19:46:25 +0800

    refactor(gutentags): consolidate config, use rg, exclude node_modules

--- init.lua
+++ init.lua
@@ -5,26 +5,5 @@ vim.g.maplocalleader = " "
 vim.g.lazyvim_check_order = false
 vim.g.lazyvim_news = false
 
--- gutentags: add to rtp + set vars BEFORE plugin auto-loads
-vim.opt.rtp:prepend(vim.fn.expand("~/.local/share/nvim/site/pack/plugins/start/vim-gutentags"))
-vim.g.gutentags_ctags_executable = vim.fn.expand("~/.local/bin/ctags")
-vim.g.gutentags_project_root = { ".git", ".hg", ".svn" }
-vim.g.gutentags_ctags_tagfile = ".tags"
-vim.g.gutentags_exclude_filetypes = {
-  "gitcommit", "gitrebase", "help", "markdown",
-  "text", "startify", "fugitive", "fugitiveblame",
-}
-vim.g.gutentags_generate_on_new = true
-vim.g.gutentags_generate_on_missing = true
-vim.g.gutentags_generate_on_write = true
-vim.g.gutentags_generate_on_empty_buffer = false
-vim.g.gutentags_cache_dir = vim.fn.expand("~/.cache/gutentags")
-vim.g.gutentags_ctags_extra_args = {
-  "--fields=+lnS",
-  "--extras=+q",
-  "--output-format=e-ctags",
-}
-vim.opt.tags:prepend(vim.fn.expand("~/.cache/gutentags/") .. "/*")
-
--- bootstrap lazy.nvim
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/plugins/gutentags.lua
+++ lua/plugins/gutentags.lua
@@ -1,14 +1,50 @@
 return {
   {
     "ludovicchabant/vim-gutentags",
-    -- Point to pre-installed local copy; skip clone
-    dir = "~/.local/share/nvim/site/pack/plugins/start/vim-gutentags",
+    dir = vim.fn.stdpath("data") .. "/site/pack/plugins/start/vim-gutentags",
     lazy = false,
     priority = 500,
+    init = function()
+      local data = vim.fn.stdpath("data")
+      local cache = vim.fn.stdpath("cache") .. "/gutentags"
+      local home = vim.fn.expand("~")
+
+      vim.g.gutentags_ctags_executable = home .. "/.local/bin/ctags"
+      vim.g.gutentags_project_root = { ".git", ".hg", ".svn" }
+      vim.g.gutentags_ctags_tagfile = ".tags"
+      vim.g.gutentags_cache_dir = cache
+
+      vim.g.gutentags_generate_on_new = true
+      vim.g.gutentags_generate_on_missing = true
+      vim.g.gutentags_generate_on_write = true
+      vim.g.gutentags_generate_on_empty_buffer = false
+
+      -- Exclude giant dirs
+      vim.g.gutentags_ctags_exclude = {
+        "node_modules", ".git", "dist", "build", "target",
+        ".next", ".nuxt", "coverage", "__pycache__",
+        "*.min.js", "*.min.css", "*.lock", "*.lockb",
+      }
+
+      -- Use rg for file listing (respects .gitignore, fast)
+      vim.g.gutentags_file_list_command = "rg --files"
+
+      vim.g.gutentags_exclude_filetypes = {
+        "gitcommit", "gitrebase", "help", "markdown",
+        "text", "startify", "fugitive", "fugitiveblame",
+      }
+
+      vim.g.gutentags_ctags_extra_args = {
+        "--fields=+lnS",
+        "--extras=+q",
+        "--output-format=e-ctags",
+      }
+
+      vim.opt.tags:prepend(cache .. "/*")
+    end,
     config = function()
-      -- Config vars are set in init.lua before lazy loads.
-      -- Now source the vimscript plugin.
-      local path = vim.fn.expand("~/.local/share/nvim/site/pack/plugins/start/vim-gutentags/plugin/gutentags.vim")
+      local path = vim.fn.stdpath("data")
+        .. "/site/pack/plugins/start/vim-gutentags/plugin/gutentags.vim"
       vim.cmd("source " .. path)
     end,
   },
